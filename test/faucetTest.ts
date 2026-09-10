b1ae1e06e8a958566d44d08af9eec228a02b84d6
azeezabidoye <azeezabidoye@yahoo.com>
2026-09-10 15:24:32 +0100

    feat: Test updated

--- test/faucetTest.ts
+++ test/faucetTest.ts
@@ -1,15 +1,23 @@
 import { expect } from "chai";
 import { network } from "hardhat";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-
+let loadFixture: any;
 const { ethers } = await network.create();
+{
+  const _hh_helpers = await import("@nomicfoundation/hardhat-network-helpers");
+  loadFixture = _hh_helpers.loadFixture ?? _hh_helpers.default?.loadFixture;
+}
+if (typeof loadFixture !== "function") {
+  loadFixture = async (fixture: (...args: any[]) => Promise<any>) => {
+    return fixture();
+  };
+}
 
 describe("Faucet", function () {
   // Deploy and set state variables for contract
   async function deployContractAndSetVariables() {
     const Faucet = await ethers.getContractFactory("Faucet");
     const faucet = await Faucet.deploy();
-    await faucet.deployed();
+    // await faucet.deployed();
 
     const [owner] = await ethers.getSigners();
 
